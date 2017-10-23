f2798fd4b69648ec1325157dfb450410b5c801a2
Alonso-PC\Alonso <alonsoaman@hotmail.com>
2017-10-23 00:32:25 -0200

    Primeiro Teste. Apenas printa no debug console.

---
+++ Source/AutoEmpty.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ICities;
+using UnityEngine;
+using ColossalFramework.Plugins;
+
+namespace AutoEmpty
+{
+    public class AutoEmpty : IUserMod
+    {
+        public String Name
+        {
+            get { return "Automatic Empty"; }
+        }
+
+        public String Description
+        {
+            get { return "Automatically starts emptying and stops whenever it finishes. Works for landfills and cemeteries"; }
+        }
+    }
+
+    public class Loader : LoadingExtensionBase
+    {
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            GameObject go = new GameObject("Test Object");
+            go.AddComponent<UnityBehavior>();
+        }
+    }
+
+    public class UnityBehavior : MonoBehaviour
+    {
+        private void Start()
+        {
+            DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "ITS WORKING!! ITS WORKING!!!");
+        }
+
+        private void FixedUpdate()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+
+        private void LateUpdate()
+        {
+            
+        }
+    }
+}
