0208d94120d3a9213b8970a3c36d88b714c9f4e9
Alonso-PC\Alonso <alonsoaman@hotmail.com>
2017-10-24 05:49:00 -0200

    Totalmente Funcional, mas precisa receber alguma otimizacao de desempenho.

--- Source/AutoEmpty.cs
+++ Source/AutoEmpty.cs
@@ -18,35 +18,105 @@ namespace AutoEmpty
         }
     }
 
+
     public class Loader : LoadingExtensionBase
     {
         public override void OnLevelLoaded(LoadMode mode)
         {
-            GameObject go = new GameObject("Test Object");
-            go.AddComponent<UnityBehavior>();
+            if (mode.Equals(LoadMode.LoadGame))
+            {
+                GameObject go = new GameObject("Test Object");
+                go.AddComponent<Monitor>();
+            }
+            
         }
     }
 
-    public class UnityBehavior : MonoBehaviour
+    public class Monitor : MonoBehaviour
     {
-        private void Start()
+        private BuildingManager _buildingManager;
+        private CemeteryAI cemeteryAI;
+        FastList<ushort> garbageBuildings, healthBuildings;
+
+        public void Start()
         {
-            DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "ITS WORKING!! ITS WORKING!!!");
+            InvokeRepeating("StartSearchAndEmpty", 5, 5);
         }
 
-        private void FixedUpdate()
+        public void StartSearchAndEmpty()
         {
-            
+            _buildingManager = ColossalFramework.Singleton<BuildingManager>.instance;
+
+            garbageBuildings = _buildingManager.GetServiceBuildings(ItemClass.Service.Garbage);
+            healthBuildings = _buildingManager.GetServiceBuildings(ItemClass.Service.HealthCare);
+
+            //OutputLogger.PrintMessage("Quantidade de Garbage buildings: " + garbageBuildings.m_size.ToString());
+
+            OutputLogger.PrintMessage("Start Scanning");
+
+            var buffer = _buildingManager.m_buildings.m_buffer;
+            int amount = 0;
+
+            foreach (ushort serviceGarbage in garbageBuildings)
+            {
+                var _buildingAi = buffer[serviceGarbage].Info.m_buildingAI;
+                if (_buildingAi is LandfillSiteAI)
+                {
+                    amount = _buildingAi.GetGarbageAmount(serviceGarbage, ref buffer[serviceGarbage]);
+                    if (amount > 7600000)
+                    {
+                        _buildingAi.SetEmptying(serviceGarbage, ref buffer[serviceGarbage], true);
+                        //OutputLogger.PrintMessage("Start Emptying Landfill: " + serviceGarbage);
+                    }
+                    else if (amount < 400000)
+                    {
+                        _buildingAi.SetEmptying(serviceGarbage, ref buffer[serviceGarbage], false);
+                        //OutputLogger.PrintMessage("Stop Emptying Landfill: " + serviceGarbage);
+                    }
+                }
+            }
+
+            foreach (ushort deathCare in healthBuildings)
+            {
+                var _buildingAi = buffer[deathCare].Info.m_buildingAI;
+                if (_buildingAi is CemeteryAI)
+                {
+                    cemeteryAI = (CemeteryAI)_buildingAi;
+                    //amount = cemeteryAI.m_graveCount;
+                    if ( cemeteryAI.IsFull(deathCare, ref buffer[deathCare]) )
+                    {
+                        cemeteryAI.SetEmptying(deathCare, ref buffer[deathCare], true);
+                       // OutputLogger.PrintMessage("Start Emptying Cemetary: " + amount);
+                    }
+                    else if ( !cemeteryAI.IsFull(deathCare, ref buffer[deathCare]) )
+                    {
+                        cemeteryAI.SetEmptying(deathCare, ref buffer[deathCare], false);
+                       // OutputLogger.PrintMessage("Stop Emptying cemetary: " + amount);
+                    }
+                }
+            }
         }
 
-        private void Update()
-        {
-            
-        }
 
-        private void LateUpdate()
+
+        public class OutputLogger
         {
-            
+            const string prefix = "[Auto Empty] ";
+
+            public static void PrintMessage(string message)
+            {
+                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, prefix + message);
+            }
+
+            public static void PrintError(string message)
+            {
+                DebugOutputPanel.AddMessage(PluginManager.MessageType.Error, prefix + message);
+            }
+
+            public static void PrintWarning(string message)
+            {
+                DebugOutputPanel.AddMessage(PluginManager.MessageType.Warning, prefix + message);
+            }
         }
     }
 }
